9034be932722d7dd6148e8273dc443be8dd7a3ee
jonrusso <russovfx@icloud.com>
2022-11-15 16:25:58 -0300

    my first programm in java

---
+++ index.java
@@ -0,0 +1,17 @@
+// This is my first programm in Java
+
+class MyFirstProgramm
+
+{
+    public static void main(String[] args)
+ {
+
+int age = 20;
+char genero = 'men';
+double weight = 70.2;
+String name = 'John Russo'; 
+String course = 'Computer Science'; 
+
+System.out.println("My name's" + name + "I'm" + age + "years old and I have" + weight + "kg of weight and I'm studying for" + Computer Science);
+ }
+}
